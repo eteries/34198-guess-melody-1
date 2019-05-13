a579601351581ce5d91cda2a77b6d422c736de5b
Сергей Яньшин <yanshin.s@spb.helix.ru>
2019-05-13 14:12:37 +0300

    add propTypes and app settings (#6)

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
+
 import {WelcomeScreen} from '../welcome-screen/welcome-screen.jsx';
 
-export const App = () => {
+export const App = (props) => {
+  const {gameTime, errorNum} = props;
+
   return (
-    <WelcomeScreen />
+    <WelcomeScreen time={gameTime} errorNum={errorNum} />
   );
+};
+
+App.propTypes = {
+  gameTime: PropTypes.number.isRequired,
+  errorNum: PropTypes.number.isRequired
 };
--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom';
 import {App} from './components/app/app.jsx';
 
 const init = () => {
+  const settings = {
+    gameTime: 5,
+    errorNum: 3,
+  };
+
   ReactDOM.render(
-      <App />,
+      <App gameTime={settings.gameTime} errorNum={settings.errorNum} />,
       document.querySelector(`.main`)
   );
 };
